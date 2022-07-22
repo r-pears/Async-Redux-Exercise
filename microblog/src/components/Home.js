c572fa04639f7a6024d4de9ed9a69d2625b27c23
Ryan <ryjen.pears@gmail.com>
2022-07-22 12:51:23 +0200

    home page added

---
+++ microblog/src/components/Home.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import TitleList from './TitleList';
+
+function Home() {
+  return (
+    <main>
+      <div>
+        Welcome to <b>Microblog</b>, our new and innovative site for
+        communicating on the Inet.
+      </div>
+      <TitleList />
+    </main>
+  );
+}
+
+export default Home;
